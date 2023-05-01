26ac3c801d59bb8d1eecb2e0dd6e8492cdc2b800
ugbebor18 <121127937+ugbebor18@users.noreply.github.com>
2023-05-01 06:28:19 -0700

    Update ec2.tf

--- tomcat/ec2.tf
+++ tomcat/ec2.tf
@@ -1,7 +1,7 @@
 # configured aws provider with proper credentials
 provider "aws" {
-  region    = "us-east-1"
-  profile   = "yusuf"
+  region    = "us-east-2"
+  profile   = "default"
 }
 
 
@@ -89,7 +89,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.small"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.tomcat_ec2_security_group.id]
-  key_name               = "devopskeypair"
+  key_name               = "ohioKP"
    user_data = "${file("install_tomcat.sh")}"
 
   tags = {
