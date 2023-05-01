4a24625993267ed907ecb087c5ff2bbf19a4bdb3
ugbebor18 <121127937+ugbebor18@users.noreply.github.com>
2023-05-01 06:25:18 -0700

    Update ec2.tf

--- JENKINS/ec2.tf
+++ JENKINS/ec2.tf
@@ -1,6 +1,6 @@
 # configured aws provider with proper credentials
 provider "aws" {
-  region    = "us-east-1"
+  region    = "us-east-2"
   profile   = "default"
 }
 
@@ -106,7 +106,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.small"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "bada"
+  key_name               = "ohioKP"
   user_data = "${file("install_jenkins.sh")}"
 
   tags = {
