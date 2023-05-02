2593e976b5ae3e2d5b2afa416203817742c7a0b2
ugbebor18 <121127937+ugbebor18@users.noreply.github.com>
2023-05-02 00:18:31 -0700

    Update ec2.tf

--- JENKINS/ec2.tf
+++ JENKINS/ec2.tf
@@ -1,123 +1,184 @@
 # configured aws provider with proper credentials
 provider "aws" {
-  region    = "us-east-2"
-  profile   = "default"
+  region     = "us-east-2"
+  profile    = "default"
 }
 
 
-# create default vpc if one does not exit
-resource "aws_default_vpc" "default_vpc" {
+# Create a VPC
 
-  tags    = {
-    Name  = "default vpc"
+resource "aws_vpc" "prodvpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "production_vpc"
   }
 }
 
+# Create a Subnet
 
-# use data source to get all avalablility zones in region
-data "aws_availability_zones" "available_zones" {}
-
-
-# create default subnet if one does not exit
-resource "aws_default_subnet" "default_az1" {
-  availability_zone = data.aws_availability_zones.available_zones.names[0]
-
-  tags   = {
-    Name = "default subnet"
-  }
-}
-
-
-# create security group for the ec2 instance
-resource "aws_security_group" "ec2_security_group" {
-  name        = "ec2 security group"
-  description = "allow access on ports 8080 and 22"
-  vpc_id      = aws_default_vpc.default_vpc.id
-
-  # allow access on port 8080
-  ingress {
-    description      = "http proxy access"
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  # allow access on port 22
-  ingress {
-    description      = "ssh access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  } 
-  ingress {
-    description      = "http access"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
+resource "aws_subnet" "prodsubnet1" {
+  vpc_id            = aws_vpc.prodvpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = true
   
-   ingress {
-    description      = "https access"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
+#Create the Internet Gateway and attach it to the VPC
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prodvpc.id
+
+  tags = {
+    Name = "New"
+  }
+}
+
+# Create a Route Table
+resource "aws_route_table" "prodroute" {
+  vpc_id = aws_vpc.prodvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
   }
 
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.gw.id
+  }
 
+  tags = {
+    Name = "RT"
+  }
+}
+
+
+#Associate the subnet with the Route Table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.prodsubnet1.id
+  route_table_id = aws_route_table.prodroute.id
+}
+
+# Create a Security Group for the EC2 Instance
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web"
+  description = "Allow webserver inbound traffic"
+  vpc_id      = aws_vpc.prodvpc.id
+
+  ingress {
+    description = "Web Traffic from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
   egress {
     from_port        = 0
     to_port          = 0
-    protocol         = -1
+    protocol         = "-1" # Any ip address/ any protocol
     cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
-
-  tags   = {
-    Name = "jenkins server security group"
-  }
-}
-
-
-# use data source to get a registered amazon linux 2 ami
-data "aws_ami" "ubuntu" {
-
-    most_recent = true
-
-    filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-    }
-
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
-
-    owners = ["099720109477"]
-}
-
-# launch the ec2 instance
-resource "aws_instance" "ec2_instance" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.small"
-  subnet_id              = aws_default_subnet.default_az1.id
-  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "ohioKP"
-  user_data = "${file("install_jenkins.sh")}"
 
   tags = {
-    Name = "jenkins_server"
+    Name = "allow_tls"
   }
 }
 
 
+# Create the EC2 instance and assign key pair
+resource "aws_instance" "firstinstance" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_web.id]
+  subnet_id              = aws_subnet.prodsubnet1.id
+  key_name               = "ohioKP"
+  availability_zone      = "us-east-2a"
+  user_data              =  "${file("install_jenkins.sh")}"
 
+
+  tags = {
+    Name = "Jenkins_Server"
+  }
+}
+
+
+resource "aws_instance" "secondinstance" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_web.id]
+  subnet_id              = aws_subnet.prodsubnet1.id
+  key_name               = "ohioKP"
+  availability_zone      = "us-east-2a"
+  user_data              =  "${file("install_tomcat.sh")}"
+  
+
+
+  tags = {
+    Name = "Tomcat_Server"
+  }
+}
+
+
+# use data source to get a registered ubuntu ami
+data "aws_ami" "ubuntu" {
+
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
 
 # print the url of the jenkins server
-output "website_url" {
-  value     = join("", ["http://", aws_instance.ec2_instance.public_ip, ":", "8080"])
+output "Jenkins_website_url" {
+  value     = join ("", ["http://", aws_instance.firstinstance.public_ip, ":", "8080"])
+  description = "Jenkins Server is firstinstance"
+}
+
+# print the url of the tomcat server
+output "Tomcat_website_url1" {
+  value     = join ("", ["http://", aws_instance.secondinstance.public_ip, ":", "8080"])
+  description = "Tomcat Server is secondinstance"
 }
